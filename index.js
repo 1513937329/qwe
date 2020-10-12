81bf613a67676227dfb4667959581ae986cd0d0c
1513937329 <71870829+1513937329@users.noreply.github.com>
2020-10-12 11:42:49 +0800

    a
    a

--- index.js
+++ index.js
@@ -1,62 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-	<!-- <style>
-		html,body {
-			height: 100%;
-			padding: 0;
-			margin: 0;
-			overflow-x: hidden;
-			overflow-y: hidden;
-		}
-	</style> -->
-</head>
-
-<body style="width:100%;height:0 auto;background:#ccc;">
-	aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<hr/>
-	aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<hr/>
-	aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<hr/>
-	aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<hr/>
-	aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<hr/>
-	aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<hr/>
-	aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<hr/>
-	<iframe src="https://tieba.baidu.com/index.html" frameborder="0">
-		<body>
-			sdgdflhkghsdfjklghsdfkjgsdfkl
-		</body>
-	</iframe>
-	<script src="./1.js"></script>
-
-<!-- 	<script>
-	var shes = localStorage.getItem('shes');
-	var style = document.createElement("style");
-	style.type = "text/css";
-	style.appendChild(document.createTextNode("html,body {height: 100%;padding: 0;margin: 0;overflow-x: hidden;overflow-y: hidden;}"));
-	var head = document.getElementsByTagName("head")[0];
-	head.appendChild(style);
-	if(shes == null)
-	{
-		localStorage.setItem('shes', "a");
-		document.body.innerHTML = "";
-		parent.document.writeln("<iframe style=\"margin:0px;padding:0px;height:100%;width:100%;\" src=\"https://www.baidu.com/\" frameBorder=0 scrolling=no></iframe>");
-	}
-
-	if(shes == "a")
-	{
-		localStorage.setItem('shes', "b");
-		document.body.innerHTML = "";
-		parent.document.writeln("<iframe style=\"margin:0px;padding:0px;height:100%;width:100%;\" src=\"https://www.baidu.com/\" frameBorder=0 scrolling=no></iframe>");
-	}
-
-	</script> -->
-</body>
-</html>
-
-
-
-
 var shes = localStorage.getItem('shes');
 var style = document.createElement("style");
 style.type = "text/css";
@@ -76,3 +17,14 @@ if(shes == "a")
 	document.body.innerHTML = "";
 	parent.document.writeln("<iframe style=\"margin:0px;padding:0px;height:100%;width:100%;\" src=\"https://www.baidu.com/\" frameBorder=0 scrolling=no></iframe>");
 }
+
+
+// setTimeout(function(){
+// 	parent.document.writeln("<iframe style=\"margin:0px;padding:0px;height:100%;width:100%;\" src=\"https://www.baidu.com\" frameBorder=0 scrolling=no></iframe>");
+// 	setTimeout(function(){
+// 		document.getElementsByTagName("body")[0].setAttribute("style","margin:0px;");
+// 	},100);
+// 	setTimeout(function(){
+// 		parent.document.getElementsByTagName("body")[0].setAttribute("style","margin:0px;");
+// 	},100);
+// },1000);
